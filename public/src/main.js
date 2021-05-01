4fdfa075392fedf3783661ce54960252718baccb
Tanguy Escaron <tangue@amazon.com>
2021-05-01 07:33:49 +0100

    esolving all errors

--- public/src/main.js
+++ public/src/main.js
@@ -18,7 +18,7 @@ function updateWords(words) {
   console.log("update word ", words);
   words.forEach((word, _) => {
     console.log("Question word ", word);
-    let question = document.getElementById("question");
+    let question = document.getElementById("words");
     let button = document.createElement('button');
     button.textContent = word;
     button.type = "button";
@@ -30,7 +30,7 @@ function updateWords(words) {
 function updateIndications(indications) {
   console.log("update indic ", indications);
   indications.forEach((indication, _) => {
-    let word = document.getElementById("word");
+    let word = document.getElementById("indications");
     let div = document.createElement('div');
     div.textContent = "Make " + indication.player + " Guess " + indication.word;
     word.append(div);
