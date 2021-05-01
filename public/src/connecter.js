4b03a7cb281011a8298db396fc3ec691d8a63dd6
AdrKacz <adrien.kaczmarek@gmail.com>
2021-05-01 06:31:19 +0100

    Wainting Room

---
+++ public/src/connecter.js
@@ -0,0 +1,17 @@
+const readyForm = document.querySelector("div#waiting-room form");
+const nameText = document.querySelector("div#waiting-room h2");
+
+readyForm.addEventListener("submit", (event) => {
+  const data = new FormData(event.target);
+
+  const playerName = data.get("player-name");
+  // TODO: Parse before sending
+
+  socket.emit("ready", playerName);
+
+  readyForm.setAttribute("class", "hidden");
+  nameText.setAttribute("class", "");
+  nameText.textContent = playerName;
+
+  event.preventDefault();
+});
--- public/src/main.js
+++ public/src/main.js
@@ -1,42 +1,38 @@
 const socket = io();
-
-function clickButton(event) {
-  io.emit('value of the button', this.textContent);
-}
-
-var pauseView = document.getElementById('pause');
-var gameView = document.getElementById('game');
-gameView.visible = false;
-
-io.emit('join');
-
-io.on('start', () => {
-    pauseView.visible = false;
-    gameView.visible = true;
-});
-
-io.on('data', (data) => {
-  updateIndications(data.indications);
-  updateWords(data.words);
-});
-
-function updateWords(words) {
-  words.forEach((word, _) => {
-    let divprinc = document.getElementById("words");
-    let button = document.createElement('button');
-    button.textContent = word;
-    button.type = "button";
-    button.name = "button";
-    divprinc.append(button);
-    button.addEventListener("click", clickButton);
-  });
-};
-
-function updateIndications(indications) {
-  indications.forEach((indication, _) => {
-    let divprinc = document.getElementById("indications");
-    let div = document.createElement('div');
-    div.textContent = "Make " + indication.player + " Guess " + indication.word;
-    divprinc.append(div);
-  });
-};
+//
+// function clickButton(event) {
+//   socket.emit('value of the button', this.textContent);
+// }
+//
+// socket.emit('join');
+//
+// socket.on('start', () => {
+//     pauseView.visible = false;
+//     gameView.visible = true;
+// });
+//
+// socket.on('data', (data) => {
+//   updateIndications(data.indications);
+//   updateWords(data.words);
+// });
+//
+// function updateWords(words) {
+//   words.forEach((word, _) => {
+//     let divprinc = document.getElementById("words");
+//     let button = document.createElement('button');
+//     button.textContent = word;
+//     button.type = "button";
+//     button.name = "button";
+//     divprinc.append(button);
+//     button.addEventListener("click", clickButton);
+//   });
+// };
+//
+// function updateIndications(indications) {
+//   indications.forEach((indication, _) => {
+//     let divprinc = document.getElementById("indications");
+//     let div = document.createElement('div');
+//     div.textContent = "Make " + indication.player + " Guess " + indication.word;
+//     divprinc.append(div);
+//   });
+// };
