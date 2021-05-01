c0fa8b90b521d5d1cb67a805c09b86995246334f
Clara.Decotignie <clara.decotignie@ensta-paristech.fr>
2021-05-01 01:15:27 +0100

    k

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const path = require("path");
 
 app.use('/static', express.static(path.join(__dirname, "public")));
 app.get('/hub', (req, res) => {
-  res.sendFile(path.join(__dirname, "hub.html"));
+  res.sendFile(path.join(__dirname, "public", "hub.html"));
 });
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
