c05a4c4f1900472ddd01a7ad0c94307d482f2614
AdrKacz <adrien.kaczmarek@gmail.com>
2021-05-01 09:37:16 +0100

    Shake Screen on bad result

--- public/src/particle.js
+++ public/src/particle.js
@@ -7,13 +7,27 @@ function clicked(event) {
   }, 500);
 };
 
+let shakeId = undefined;
+function shakeScreen() {
+  if (shakeId !== undefined) {
+    return;
+  };
+
+  document.querySelector("div#main").setAttribute("class", "shake");
+  shakeId = setTimeout(() => {
+    document.querySelector("div#main").setAttribute("class", "");
+    shakeId = undefined;
+  }, 500);
+};
+
 
 document.querySelector("div#words").addEventListener("click", clicked);
 
 socket.on("result", (result) => {
   if (result === true) {
-
+    console.log("Good game");
   } else if (result === false) {
-
+    console.log("Bad game");
+    shakeScreen();
   };
 });
